1ff98cf7e37da71d6d178d5dfba6410976d15169
sergiocotiazure20265-ops <sergiocotiazure20265@gmail.com>
2026-08-16 10:35:20 -0300

    fix: ajusta os testes para um ambiente com banco de memoria h2

--- src/test/java/br/com/cotiinformatica/api_java_clientes/ApiJavaClientesApplicationTests.java
+++ src/test/java/br/com/cotiinformatica/api_java_clientes/ApiJavaClientesApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.cotiinformatica.api_java_clientes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiJavaClientesApplicationTests {
 
 	@Test
